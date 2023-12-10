7433d581cc81de3d8ad1e1adca15a3db71769731
hariprog <147419217+hariprog@users.noreply.github.com>
2023-12-10 14:52:26 +0530

    #include<stdio.h> int main() {     int i,j,n,k;     scanf("%d",&n);     for(i=n;i>=1;i--) {         for(j=0;j<n-i;j++) {             printf(" ");         }         for(k=i;k>0;k--)         printf("%c ",64+i);         printf(" ");     } }

---
+++ Pattern_7.4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int i,j,n,k;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--) {
+        for(j=0;j<n-i;j++) {
+            printf(" ");
+        }
+        for(k=i;k>0;k--)
+        printf("%c ",64+i);
+        printf("
+");
+    }
+}
