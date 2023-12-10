6ef6d4b89d8c3e2258261334f0734e71fd9c6791
hariprog <147419217+hariprog@users.noreply.github.com>
2023-12-10 14:22:29 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++) {         for(j=0;j<n-i;j++) {             printf(" ");         }         for(j=i;j>=1;j--)         printf("%d ",n-i+1);         printf(" ");     } }

---
+++ Basic_Pattern_6.2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++) {
+        for(j=0;j<n-i;j++) {
+            printf(" ");
+        }
+        for(j=i;j>=1;j--)
+        printf("%d ",n-i+1);
+        printf("
+");
+    }
+}
