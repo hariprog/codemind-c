7ddd9fe68882732269d6262787f108f2cf137c02
hariprog <147419217+hariprog@users.noreply.github.com>
2023-12-12 09:35:17 +0530

    #include<stdio.h> int main() {     int x,sum=0;     scanf("%d",&x);     int a[x];     for(int i=0;i<=x-1;i++) {         scanf("%d",&a[i]);     }
        for(int j=0;j<x;j++) {
            if(a[j]%2==0)
            sum+=a[j];
        }
        printf("%d",sum);
    }

---
+++ Even_Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int x,sum=0;
+    scanf("%d",&x);
+    int a[x];
+    for(int i=0;i<=x-1;i++) {
+        scanf("%d",&a[i]);
+    }
+  
+    for(int j=0;j<x;j++) {
+        if(a[j]%2==0)
+        sum+=a[j];
+    }
+    printf("%d",sum);
+}
