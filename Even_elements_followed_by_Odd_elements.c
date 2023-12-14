9be3755b411cb0fa56dbebac38b4d2ee6ffade0d
hariprog <147419217+hariprog@users.noreply.github.com>
2023-12-14 11:44:04 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     int a[x];     for(int i=0;i<x;i++) {         scanf("%d",&a[i]);     }     for(int j=0;j<x;j++) {         if(a[j]%2==0)         printf("%d ",a[j]);     }     for(int k=0;k<x;k++) {         if(a[k]%2!=0)         printf("%d ",a[k]);     } }

---
+++ Even_elements_followed_by_Odd_elements.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    int x;
+    scanf("%d",&x);
+    int a[x];
+    for(int i=0;i<x;i++) {
+        scanf("%d",&a[i]);
+    }
+    for(int j=0;j<x;j++) {
+        if(a[j]%2==0)
+        printf("%d ",a[j]);
+    }
+    for(int k=0;k<x;k++) {
+        if(a[k]%2!=0)
+        printf("%d ",a[k]);
+    }
+}
