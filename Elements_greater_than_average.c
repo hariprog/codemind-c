6bf09ea67dbf10c3f743b6a2a543b2006225c28d
hariprog <147419217+hariprog@users.noreply.github.com>
2023-12-14 11:08:00 +0530

    #include<stdio.h> int main() {     int x,sum=0,count=0;     scanf("%d",&x);     int a[x];     for(int i=0;i<x;i++) {         scanf("%d",&a[i]);         sum+=a[i];     }     int avg=sum/x;     for(int j=0;j<x;j++) {         if(avg<=a[j])         count++;     }     printf("%d",count); }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    int x,sum=0,count=0;
+    scanf("%d",&x);
+    int a[x];
+    for(int i=0;i<x;i++) {
+        scanf("%d",&a[i]);
+        sum+=a[i];
+    }
+    int avg=sum/x;
+    for(int j=0;j<x;j++) {
+        if(avg<=a[j])
+        count++;
+    }
+    printf("%d",count);
+}
