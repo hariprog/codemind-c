d2a844a5242744d7734e11592b881568c61c9001
hariprog <147419217+hariprog@users.noreply.github.com>
2023-12-19 11:42:38 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     int a[x];     for(int i=0;i<x;i++) {          scanf("%d",&a[i]);     }     for(int j=x-1;j>0;j--) {       if(a[j]%2!=0)       printf("%d",j);       break; } }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int x;
+    scanf("%d",&x);
+    int a[x];
+    for(int i=0;i<x;i++) {
+         scanf("%d",&a[i]);
+    }
+    for(int j=x-1;j>0;j--) {
+      if(a[j]%2!=0)
+      printf("%d",j);
+      break;
+}
+}
