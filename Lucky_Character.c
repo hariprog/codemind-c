45d05b816fb2fafa88561a03ab0c7139fb0915ba
hariprog <147419217+hariprog@users.noreply.github.com>
2023-12-26 10:22:50 +0530

    #include<stdio.h> #include<string.h> int main() {     char a[100];     int count=0;     fgets(a,100,stdin);
        for(int j=0;a[j]!=NULL;j++) {
            printf("%c",a[6]);
            break;
        }
    }

---
+++ Lucky_Character.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char a[100];
+    int count=0;
+    fgets(a,100,stdin);
+    
+    for(int j=0;a[j]!=NULL;j++) {
+        printf("%c",a[6]);
+        break;
+    }
+}
