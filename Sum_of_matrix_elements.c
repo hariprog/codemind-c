2f0b896c6195ff37f914ffaf91cc1cb3cee4ee55
hariprog <147419217+hariprog@users.noreply.github.com>
2023-12-30 18:44:32 +0530

    #include<stdio.h> int main() {     int x,y,sum=0;     scanf("%d %d",&x,&y);     int a[x][y];     for(int i=0;i<x;i++) {         for(int j=0;j<y;j++) {             scanf("%d",&a[i][j]);             sum+=a[i][j];         }     }     printf("%d",sum); }

---
+++ Sum_of_matrix_elements.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int x,y,sum=0;
+    scanf("%d %d",&x,&y);
+    int a[x][y];
+    for(int i=0;i<x;i++) {
+        for(int j=0;j<y;j++) {
+            scanf("%d",&a[i][j]);
+            sum+=a[i][j];
+        }
+    }
+    printf("%d",sum);
+}
