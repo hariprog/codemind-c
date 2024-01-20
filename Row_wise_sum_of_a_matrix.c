db1c12c23afa1de0e89a705d604c9705ca1b8567
hariprog <147419217+hariprog@users.noreply.github.com>
2024-01-20 21:46:56 +0530

    #include<stdio.h> int main() {     int x,y,sum=0;     scanf("%d %d",&x,&y);     int a[x][y];     for(int i=0;i<x;i++) {         for(int j=0;j<y;j++) {             scanf("%d",&a[i][j]);         }     }     for(int i=0;i<x;i++) {         for(int j=0;j<y;j++) {             sum+=a[i][j];         }         printf("%d ",sum);         sum=0;     }
    }

---
+++ Row_wise_sum_of_a_matrix.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int x,y,sum=0;
+    scanf("%d %d",&x,&y);
+    int a[x][y];
+    for(int i=0;i<x;i++) {
+        for(int j=0;j<y;j++) {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(int i=0;i<x;i++) {
+        for(int j=0;j<y;j++) {
+            sum+=a[i][j];
+        }
+        printf("%d ",sum);
+        sum=0;
+    }
+    
+}
