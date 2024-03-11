cd66831306fd7b87d3e9993f87457d978e23f4c3
hariprog <147419217+hariprog@users.noreply.github.com>
2024-03-11 20:19:44 +0530

    #include<stdio.h> int main() {     int x;      scanf("%d",&x);      int a[x];      for(int i=0;i<x;i++){          scanf("%d",&a[i]);      }      int k=1;        for(int i=1;i<x;i+=2){            if(a[i]%2==0) {                k=0;            }        }     if(k) {         printf("True");     }     else {     printf("False");     } }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+    int x;
+     scanf("%d",&x);
+     int a[x];
+     for(int i=0;i<x;i++){
+         scanf("%d",&a[i]);
+     }
+     int k=1;
+       for(int i=1;i<x;i+=2){
+           if(a[i]%2==0) {
+               k=0;
+           }
+       }
+    if(k) {
+        printf("True");
+    }
+    else {
+    printf("False");
+    }
+}
